88b36ffcfd54c0f54e08f57340659f7fd43d4fe7
Jiyao Liu <63041664+Jiyao17@users.noreply.github.com>
2022-10-13 16:39:05 -0400

    working

--- source/fl/node.py
+++ source/fl/node.py
@@ -2,23 +2,102 @@
 # depict the hardware structure of fl
 # how the devices are connected to each other
 
-class Node:
-    class Config:
-        def __init__(self, id: str, neighbors: list, tasks: list):
-            self.id = id
-            self.neighbors = neighbors
+from multiprocessing import Process, Queue, 
+from multiprocessing.connection import PipeConnection
+from copy import deepcopy
+import enum
+
+import numpy as np
+
+from task import TrainerTask, AggregatorTask
+
+
+class Cammand(enum.Enum):
+    # Cammands
+    CLIENT_UPDATE = 0
+    CLIENT_REPORT = 1
+    CLIENT_QUIT = 10
+
+    AGGREGATOR_UPDATE = 11
+    AGGREGATOR_REPORT = 12
+    AGGREGATOR_QUIT = 20
+
+
+class Trainer():
+    def __init__(self, task: TrainerTask, parent_pipe: PipeConnection,):
+        # pipe for communication with aggregator
+        self.parent_pipe = parent_pipe
+        # everything related to training
+        self.task = task
+
+    def work_loop(self):
+        command = self.parent_pipe.recv()
+        while command != Cammand.CLIENT_QUIT:
+
+            if command == Cammand.CLIENT_UPDATE:
+                self.task.train()
+            elif command == Cammand.CLIENT_REPORT:
+                update = self.task.report_update()
+                self.parent_pipe.send(update)
             
-    def __init__(self, config: Config):
-        self.config = config
+            command = self.parent_pipe.recv()
+            
+
+class Aggregator():
     
+    def __init__(self, task: AggregatorTask, trainer_pipes: 'list[PipeConnection]',  
+            final_aggregator=False, parent_pipe: PipeConnection=None, verbose=False
+            ):
+        # everything related to aggregation
+        self.task = task
+
+        # pipes for communication with trainers
+        self.pipes = trainer_pipes
+        self.final_aggregator = final_aggregator
+        self.parent_pipe = parent_pipe
+        if self.final_aggregator is True:
+            assert self.parent_pipe is None, 'Final aggregator shouldn\'t have parent pipe'
+        else:
+            assert self.parent_pipe is not None, 'Non-final aggregator should have parent pipe'
+        self.verbose = verbose
+
+        # activated trainers in each round
+        self.activation_list = np.full((len(self.pipes), ), dtype=bool, fill_value=False)
+        # responeded trainers in each round
+        self.response_list = np.full((len(self.pipes), ), dtype=bool, fill_value=False)
+        self.update_list = [None] * len(self.pipes)
+
+    def work_loop(self):
+        if self.final_aggregator is False:
+            command = self.parent_pipe.recv()
+            while command != Cammand.AGGREGATOR_QUIT:
+                if command == Cammand.AGGREGATOR_UPDATE:
+                    self.update()
+                elif command == Cammand.AGGREGATOR_REPORT:
+                    self.report()
+
+
+                command = self.parent_pipe.recv()
+
     def update(self):
-        pass
-    
+        # reset response list
+        self.response_list = np.full((len(self.pipes), ), dtype=bool, fill_value=False)
 
-class Trainer(Node):
-    def __init__(self, config: Node.Config):
-        super().__init__(config)
+        # send update command to all trainers
+        for pipe in self.pipes:
+            pipe.send(Cammand.CLIENT_UPDATE)
+            # pipe.send(Cammand.CLIENT_REPORT)
+        
+        # wait for trainers' response
+        while not np.all(self.response_list):
+            for i, pipe in enumerate(self.pipes):
+                if self.response_list[i] is False and pipe.poll():
+                    self.update_list[i] = pipe.recv()
+                    self.response_list[i] = True
 
-class Aggregator(Node):
-    def __init__(self, config: Node.Config):
-        super().__init__(config)
+        self.task.aggregate(self.update_list)
+        
+
+
+    def report(self):
+        state_dict = self.task.report_update()
--- source/fl/task.py
+++ source/fl/task.py
@@ -1,26 +1,57 @@
 
 import enum
 
+from torch import nn
+from torch.utils.data.dataset import Dataset
+
+
 # depict the training structure of fl
 
 # class TaskName(enum.Enum):
 #     # Task names
 #     pass
 
-class Task:
-    class Config:
-        def __init__(self, name: str, dataset: str, model: str, epochs: int, batch_size: int):
-            self.name = name
-            self.dataset = dataset
-            self.model = model
-            self.epochs = epochs
-            self.batch_size = batch_size
 
-    def __init__(self, config: Config):
-        self.config = config
+
+class TrainerTask():
+    def __init__(self, model: nn.Module, dataset: Dataset, 
+        epochs: int, lr: float, batch_size: int,
+        device: str
+        ):
+        self.model = model
+        self.dataset = dataset
+
+        self.lr = lr
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.device = device
+
 
     def train(self):
         pass
 
+    def set_model_by_state_dict(self, state_dict: dict):
+        self.model.load_state_dict(state_dict)
+    
+    def report_update(self):
+        return self.model.state_dict()
+
     def save(self, path: str):
+        pass
+
+
+
+class AggregatorTask():
+
+    def __init__(self, model: nn.Module, dataset: Dataset, ):
+        self.model = model
+        self.dataset = dataset
+
+    def aggregate(self, ):
+        pass
+
+    def save(self, path: str):
+        pass
+
+    def update(self, ):
         pass
